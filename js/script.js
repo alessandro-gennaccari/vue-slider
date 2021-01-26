08c37c2f519cac2140fea2616f174b944638070f
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-26 13:36:52 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var slider = new Vue({
+  el : '#slider',
+  data : {
+    imgs: [
+      'img/1.jpg',
+      'img/2.jpg',
+      'img/3.jpg',
+      'img/4.jpg',
+      'img/5.jpg'
+    ],
+    counter: 0
+  }
+});
