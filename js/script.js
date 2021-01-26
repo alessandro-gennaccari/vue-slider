816e602f5aaf368cef14afd94feb14698f381dfe
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-26 16:22:05 +0100

    add.keydown-arrow

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ var slider = new Vue({
     ],
     counter: 0
   },
+  // Al Keydown dei stati direzionali destra e sinistra lo slider eseguira le funzioni
+  created() {
+    addEventListener('keydown', function(e) {
+      (e.key == 'ArrowLeft') ? slider.goPrev() : '';
+      (e.key == 'ArrowRight') ? slider.goNext() : '';
+    });
+  },
   methods: {
     goNext(){
       (this.counter == (this.imgs.length - 1)) ? this.counter = 0 : this.counter++;
